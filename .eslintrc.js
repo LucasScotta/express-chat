5fddbd69c1ebb5d913c4f446b29c4dd3a9d7900c
Lucas <lucasmanuelscotta@gmail.com>
2020-07-16 21:15:33 -0300

    proyecto sobre express

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+/* istanbul ignore file */
+/* eslint-disable */
+
+module.exports = {
+
+  extends: 'eslint:recommended',
+  root: true,
+
+  env: {
+    node: true,
+    es6: true,
+  },
+
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+
+  // rules: {
+
+  //   'semi': [ 2, 'never' ],
+  //   'comma-dangle': [ 2, 'always-multiline' ],
+
+  //   'no-extra-semi': 2,
+  //   'no-console': 2,
+
+  //   'no-undef': 2,
+  //   'no-unused-vars': [ 2, {
+  //     vars: 'all',
+  //     args: 'after-used',
+  //     caughtErrors: 'none',
+  //     ignoreRestSiblings: true,
+  //     argsIgnorePattern: '^_',
+  //   }],
+  //   'no-useless-escape': 2,
+
+  //   'require-atomic-updates': 1,
+  // }
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+// Si tu codigo hace require debe estar declarado como dependencia en el package.json
+const PORT = 8080
+const express = require('express')
+const app = express()
+// const path = require('path')
+// const dynamic = require('./lib/dynamic')
+const router = require('./lib/router')
+
+app.use('/api', router())
+
+app.use('/ping', (req, resp) => {
+	resp.send('pong')
+})
+
+app.use(express.static('public'))
+
+app.use('/$', (req, resp) => {
+	resp.send('hola')
+})
+
+app.listen(PORT)
+console.log('listen on localhost:8080')

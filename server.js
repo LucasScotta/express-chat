a2f2a74d8441c77a1c1192198a2c0eea2d347d08
Lucas <lucasmanuelscotta@gmail.com>
2020-08-02 17:35:38 -0300

    test de is-logged borrado

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 // Si tu codigo hace require debe estar declarado como dependencia en el package.json
 const PORT = 8080
 // const path = require('path')
-const express = require('express')
-const router = require('./lib/router')
 
 const app = require('./app')
 
-app.use('/api', router())
-
-app.use('/ping', (req, resp) => {
-  resp.send('pong')
-})
-
-app.use(express.static('public'))
-
-app.use('/$', (req, resp) => {
-  resp.send('Bienvenido')
-})
 
 app.listen(PORT)
 console.log('listen on localhost:8080')

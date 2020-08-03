0e4a6c653c524b74b8c7da7c1a5a506fea11c49a
Lucas <lucasmanuelscotta@gmail.com>
2020-08-03 20:17:10 -0300

    test login/create

---
+++ test/lib/api-login-create.js
@@ -0,0 +1,53 @@
+const request = require('../server/request')
+const unlink = require('./unlink')
+const config = require('../config')
+
+describe("when path is '/api/login/create'", () => {
+  describe("when there's no user or pass", () => {
+    describe("when there's no user", () => {
+      it("should response a 417", (done) => {
+        request()
+          .get('/api/login/create')
+          .query({ user: 'asdsa'})
+          .expect(417)
+          .end(done)
+      })
+    })
+    describe("when there's no pass", () => {
+      it("should response a 417", (done) => {
+        request()
+          .get('/api/login/create')
+          .query({ pass: 'asdaa'})
+          .expect(417)
+          .end(done)
+      })
+    })
+  })
+
+  describe("when user exists", () => {
+    it("should response a 417", (done) => {
+      request()
+        .get('/api/login/create')
+        .query({ user: 'lucas'})
+        .query({ pass: 'soyio'})
+        .expect(417)
+        .end(done)
+    })
+  })
+
+  describe("when user doesn't exists", () => {
+    it("should create a new user", (done) => {
+      request()
+        .get('/api/login/create')
+        .query({ user: 'user'})
+        .query({ pass: 'pass'})
+        .expect(200)
+        .end(done)
+    })
+    //FALTA BORRAR EL ARCHIVO (after?)
+    after((done) => {
+      console.log(config.path)
+      unlink(config.path + '/user.json', done)
+    })
+  })
+})

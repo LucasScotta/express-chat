76c0806dc8ad9e68148ac52e9793a1f286f1bdb8
Lucas <lucasmanuelscotta@gmail.com>
2020-08-12 16:13:13 -0300

    chat/Get&Post

--- app.js
+++ app.js
@@ -65,3 +65,122 @@ exports.get('/smf', (req, resp) => {
     waiting.push(resp)
   }
 })
+
+exports.get('/debug', (req, resp) => {
+  console.log('clients', clientsWaiting.map(client => client.req.sesion))
+  console.log('messages', messagesQueue)
+  console.log('number of clients', clients)
+  resp.send(200)
+})
+
+const clientsWaiting = []
+const messagesQueue = []
+
+exports.post('/msg', (req, resp) => {
+
+  const msg = req.body.msg
+  if (msg === "") resp.sendStatus(200)
+  else {
+    messagesQueue.push({
+      msg,
+      date: Date.now(),
+      sent:0,
+      clients,
+    })
+    setImmediate(sendMsg)
+    resp.sendStatus(200)
+  }
+})
+
+const clientNeeds = (client, msg) => {
+  return client.req.sesion.lastDate < msg.date
+}
+
+const canBeRemoved = (msg) => {
+  return msg.sent === msg.clients || msg.date + 30 * 1000 < Date.now()
+}
+
+const sendMsg = () => {
+  const clientsStillWaiting = []
+  while (clientsWaiting.length > 0) {
+
+    const client = clientsWaiting.pop()
+    const batch = messagesQueue.filter(msg => clientNeeds(client, msg))
+    if (batch.length > 0) {
+      client.resp.send(batch)
+      delete client.req.sesion.connected
+      batch.forEach(msg => msg.sent += 1)
+      client.req.sesion.lastDate = batch[batch.length - 1].date
+    }
+    else {
+      clientsStillWaiting.push(client)
+    }
+  }
+  clientsStillWaiting.forEach(client => clientsWaiting.push(client))
+
+  for (let i = 0; i < messagesQueue.length;/*empty on purpose*/) {
+    if (canBeRemoved(messagesQueue[i])) {
+      messagesQueue.splice(i, 1)
+    }
+    else {
+      i += 1
+    }
+  }
+}
+
+let clients = 0
+
+const clearClients = (client) => {
+
+  if (client) {
+    const index = clientsWaiting.indexOf(client)
+    delete client.req.sesion.date
+    delete client.req.sesion.connected
+    clientsWaiting.splice(index, 1)
+    clients -= 1
+  }
+  else if (clientsWaiting.length > 0) {
+    clientsWaiting.forEach((client, index, arr) => {
+      if (client.req.sesion.date + 10 * 1000 < Date.now()) {
+        delete arr[index].req.sesion.date
+        delete arr[index].req.sesion.connected
+        arr.splice(index, 1)
+        clients -= 1
+      }
+    })
+  }
+  setTimeout(() => clearClients(), 60 * 3 * 1000)
+}
+
+clearClients()
+
+const isNewClient = (req) => {
+  return typeof req.sesion.lastDate !== 'number'
+}
+
+exports.get('/msg', (req, resp) => {
+  // Si el cliente es nuevo
+  // Si el cliente recibio todos los mensajes
+  // Si el cliente, ya esta en espera
+  if (isNewClient(req)) {
+    //seteo date, sumo un cliente y lo pongo en espera
+    req.sesion.lastDate = Date.now()
+    clients += 1
+    // Aca deberia ir como 'name' el nombre de usuario (creo)
+    req.sesion.connected = true
+    // Aca deberia ir como 'name' el nombre de usuario (creo)
+    clientsWaiting.push({ req, resp })
+  }
+  else {
+    if (!req.sesion.connected) {
+      req.sesion.lastDate = Date.now()
+    // Aca deberia ir como 'name' el nombre de usuario (creo)
+      req.sesion.connected = true
+    // Aca deberia ir como 'name' el nombre de usuario (creo)
+      clientsWaiting.push({ req, resp })
+   }
+   else {
+    resp.send(417)
+   }
+  }
+})

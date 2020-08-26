9f2f8a1c8052ea0f82fdead64ba2852a505aa1f0
Lucas <lucasmanuelscotta@gmail.com>
2020-08-26 03:35:30 -0300

    getUser en Feed

--- test/lib/chat/feed.js
+++ test/lib/chat/feed.js
@@ -1,4 +1,12 @@
 const Feed = require('../../../lib/chat/feed')
 
 describe('/lib/chat/feed', () => {
+  it('should return its user', () => {
+    const user = 'lucas'
+    const feed = new Feed(user)
+    const result = feed.getUser()
+    expect(result)
+      .to.be.equal(user)
+  })
+
 }

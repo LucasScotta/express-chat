ba9375d347a7f46d4bfb9e1fc9ac9b6987be7391
Lucas <lucasmanuelscotta@gmail.com>
2020-08-26 03:40:25 -0300

    getId en Room

--- test/lib/chat/room.js
+++ test/lib/chat/room.js
@@ -2,4 +2,13 @@ const Room = require('../../../lib/chat/room')
 const Feed = require('../../../lib/chat/feed')
 
 describe('/lib/chat/room', () => {
+  it('should auto-assign room ids', () => {
+    const room1 = new Room()
+    const room2 = new Room()
+    const id1 = room1.getId()
+    const id2 = room2.getId()
+    expect(id1)
+      .to.be.a('number')
+      .to.be.below(id2)
+  })
 }

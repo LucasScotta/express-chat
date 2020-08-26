6264480d7fbd39945e3f5836ebe5e22a3493b18e
Lucas <lucasmanuelscotta@gmail.com>
2020-08-26 03:28:30 -0300

    removeRoom en Chat

--- test/lib/chat/chat.js
+++ test/lib/chat/chat.js
@@ -33,4 +33,26 @@ describe('lib/chat/chat.js', () => {
       .to.have.length(3)
       .to.have.members([r1, r2, r3])
   })
+
+  it('should delete a room, existing', () => {
+    const chat = new Chat()
+    const r1 = chat.createRoom()
+    const r2 = chat.createRoom()
+    const r3 = chat.createRoom()
+    const removed = chat.removeRoom(r2)
+    expect(removed)
+      .to.be.equal(true)
+    const rooms = chat.getRooms()
+    expect(rooms)
+      .to.have.length(2)
+      .to.have.members([r1, r3])
+  })
+  it('sould delete a room, non existing', () => {
+    const chat1 = new Chat()
+    const chat2 = new Chat()
+    const room = chat1.createRoom()
+    const removed = chat2.removeRoom(room)
+    expect(removed)
+      .to.be.equal(false)
+  })
 }

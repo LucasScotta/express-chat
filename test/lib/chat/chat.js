2251ceeb5abdbbd11aa26eed3095078ae22900e1
Lucas <lucasmanuelscotta@gmail.com>
2020-08-26 03:22:32 -0300

    createRoom en Chat

--- test/lib/chat/chat.js
+++ test/lib/chat/chat.js
@@ -2,4 +2,9 @@ const Chat = require('../../../lib/chat/chat')
 const Room = require('../../../lib/chat/room')
 
 describe('lib/chat/chat.js', () => {
+  it('should create a room', () => {
+    const chat = new Chat()
+    const room = chat.createRoom()
+    expect(room).to.be.instanceof(Room)
+  })
 }

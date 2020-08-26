f0a0b125ff0f99b8d19b7090332a3b39de1a9bde
Lucas <lucasmanuelscotta@gmail.com>
2020-08-26 03:36:32 -0300

    belongsTo en Feed

--- test/lib/chat/feed.js
+++ test/lib/chat/feed.js
@@ -9,4 +9,19 @@ describe('/lib/chat/feed', () => {
       .to.be.equal(user)
   })
 
+  it('should belong to user', () => {
+    const user = 'lucas'
+    const feed = new Feed(user)
+    const belongs = feed.belongsTo(user)
+    expect(belongs)
+      .to.be.true
+  })
+
+  it('should not belong to user', () => {
+    const user = 'lucas'
+    const feed = new Feed('martin')
+    const belongs = feed.belongsTo(user)
+    expect(belongs)
+      .to.be.false
+  })
 }

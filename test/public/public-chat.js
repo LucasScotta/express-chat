cbd52c7ad42555309cc9834190aa09b05fc6e562
Lucas <lucasmanuelscotta@gmail.com>
2020-09-02 19:57:03 -0300

    test de chat en public

---
+++ test/public/public-chat.js
@@ -0,0 +1,32 @@
+const request = require('../server/request')
+
+describe('When path is chat.html', () => {
+  describe('When logged', () => {
+    const agent = request.agent()
+    before((done) => {
+      agent
+        .post('/api/login')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .send('user=lucas')
+        .send('pass=pass')
+        .expect(302)
+        .expect('Content-Type', /text/)
+        .end(done)
+    })
+    it.only('should response the chat', (done) => {
+      agent
+        .get('/chat.html')
+        .expect(200, /chat/, /messages/, /send/, /message/)
+        .end(done)
+    })
+  })
+
+  describe('When not logged', () => {
+    it.only('should response a 302 to the loggin form', (done) => {
+      request()
+        .get('/chat.html')
+        .expect(302, /Redirecting to \/loggin/)
+        .end(done)
+    })
+  })
+})

13a741f16685f77bf2517b5064399249f946c885
Lucas <lucasmanuelscotta@gmail.com>
2020-09-01 22:20:48 -0300

    spy agregado en clase Room

--- test/intgr/chat/room.js
+++ test/intgr/chat/room.js
@@ -1,8 +1,37 @@
 const Room = require('../../../lib/axios-routes/chat/room')
 
-discribe('intgr/chat/room', () => {
+describe('intgr/chat/room', () => {
 
   it('should send messages to some users', () => {
+    const room = new Room({name: 'lucas'})
+    const feed1 = room.addUser('lucas')
+    const feed2 = room.addUser('martin')
 
+    const spy = chai.spy()
+
+    const payload1 = {1: 1, 2: 2, 3: 3}
+    const payload2 = {a:'a',b:'b',c:'c'}
+    const payload3 = {a:1,b:2,c:3}
+
+    feed1.on('msg', spy)
+    feed2.on('msg', spy)
+
+    feed1.emit('msg', payload1)
+    feed2.emit('msg', payload1)
+
+    feed1.emit('msg', payload2)
+    feed2.emit('msg', payload2)
+
+    feed1.emit('msg', payload3)
+    feed2.emit('msg', payload3)
+
+
+    expect(spy)
+      .on.nth(1).with(payload1)
+      .on.nth(2).with(payload1)
+      .on.nth(3).with(payload2)
+      .on.nth(4).with(payload2)
+      .on.nth(5).with(payload3)
+      .on.nth(6).with(payload3)
   })
 })

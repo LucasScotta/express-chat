ce01d62ce502c6b2b27b2e2a417e33e079304213
Lucas <lucasmanuelscotta@gmail.com>
2020-09-01 21:54:58 -0300

    test de spy de emicion de eventos en clase Feed agregado

--- test/lib/chat/feed.js
+++ test/lib/chat/feed.js
@@ -32,4 +32,24 @@ describe('/lib/chat/feed', () => {
     expect(feed.emit('send-message'))
       .to.be.false
   })
+
+  it('should send messages', () => {
+    const feed = new Feed('lucas')
+    const spy = chai.spy()
+    feed.on('message', spy)
+    const payload1 = {
+      a: 1,
+      b: 2,
+      c: 3,
+    }
+    const payload2 = ['a', 'b', 'c']
+    const payload3 = 'assd'
+    feed.emit('message', payload1)
+    feed.emit('message', payload2)
+    feed.emit('message', payload3)
+    expect(spy)
+      .on.nth(1).with(payload1)
+      .on.nth(2).with(payload2)
+      .on.nth(3).with(payload3)
+  })
 })

bd0577ab8d8a4d81ad2869e192873fc7c6c5f74a
Lucas <lucasmanuelscotta@gmail.com>
2020-09-03 05:33:54 -0300

    create en clase Message

---
+++ test/lib/chat/message.js
@@ -0,0 +1,32 @@
+const Message = require('../../../lib/axios-routes/chat/message')
+
+describe('lib/chat/message', () => {
+
+  it('should create messages', () => {
+    const message = new Message()
+    const msge = {message: '1', roomId: 2}
+    const msg = message.create(msge)
+    expect(message)
+      .to.be.instanceof(Message)
+    expect(msg)
+      .to.be.equal(msge)
+  })
+  it('should throw on invalid message', () => {
+    const message = new Message()
+    const msg1 = () => message.create({message: true, roomId:2})
+    const msg2 = () => message.create({roomId:2})
+    expect(msg1)
+      .to.throw(/Invalid message, expected string/, /but got boolean/)
+    expect(msg2)
+      .to.throw(/Invalid message, expected string/, /but got null/)
+  })
+  it('should throw on invalid room id', () => {
+    const message = new Message()
+    const msg1 = () => message.create({message: 'asd', roomId: true})
+    const msg2 = () => message.create({message: 'asd'})
+    expect(msg1)
+      .to.throw(/Invalid room id, expected number but got boolean/)
+    expect(msg2)
+      .to.throw(/invalid data: missing room id/)
+  })
+})

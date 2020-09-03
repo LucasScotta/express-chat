6f78bf482621d30f2f5c9b1df383171b9fc2e9c9
Lucas <lucasmanuelscotta@gmail.com>
2020-09-03 04:36:49 -0300

    test bug

--- test/intgr/chat/room.js
+++ test/intgr/chat/room.js
@@ -5,7 +5,8 @@ describe('intgr/chat/room', () => {
   it('should send messages to some users', () => {
     const room = new Room({name: 'lucas'})
 
-    const spy = chai.spy()
+    const spy1 = chai.spy()
+    const spy2 = chai.spy()
 
     const payload1 = {1: 1, 2: 2, 3: 3}
     const payload2 = {a:'a',b:'b',c:'c'}
@@ -14,22 +15,29 @@ describe('intgr/chat/room', () => {
     const feed1 = room.addUser('lucas')
     const feed2 = room.addUser('martin')
 
-    feed1.on('msg', spy)
-    feed2.on('msg', spy)
+    feed1.on('message', spy1)
+    feed2.on('message', spy2)
+    room.publishMessage(payload1)
+    room.publishMessage(payload2)
+    room.publishMessage(payload3)
 
-    feed1.emit('msg', payload1)
-    feed2.emit('msg', payload1)
-    feed1.emit('msg', payload2)
-    feed2.emit('msg', payload2)
-    feed1.emit('msg', payload3)
-    feed2.emit('msg', payload3)
-    expect(spy)
-      .to.be.called(6)
+    expect(spy1)
+      .to.be.called(3)
       .on.nth(1).with(payload1)
-      .on.nth(2).with(payload1)
-      .on.nth(3).with(payload2)
-      .on.nth(4).with(payload2)
-      .on.nth(5).with(payload3)
-      .on.nth(6).with(payload3)
+      .on.nth(2).with(payload2)
+      .on.nth(3).with(payload3)
+    expect(spy2)
+      .to.be.called(3)
+      .on.nth(1).with(payload1)
+      .on.nth(2).with(payload2)
+      .on.nth(3).with(payload3)
   })
 })
+
+
+
+/*
+1. Con 2 usuarios loggeados.
+2. Con una diferencia de 15s.
+3. Hacer el recorrido completo desde loggearse hasta recibir el mensaje haciendo expects detalladamente.
+*/

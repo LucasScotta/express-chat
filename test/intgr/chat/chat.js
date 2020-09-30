2d8b8bd3417c4eaf4509d9a613d7c03d8e14b8d2
Lucas <lucasmanuelscotta@gmail.com>
2020-09-30 19:54:25 -0300

    arregle tests que usan chat.createRoom (de Sync a Async)

--- test/intgr/chat/chat.js
+++ test/intgr/chat/chat.js
@@ -1,17 +1,19 @@
 const Chat = require('../../../lib/routes/chat')()
 const Message = require('../../../lib/routes/chat/message')
+const timeout = {timeoutInMillis: 30000}
 xdescribe('intgr/chat', () => {
   it('should send msg to all the feeds', () => {
     const chat = new Chat()
     const message = new Message()
-    const room = chat.createRoom({name: ''})
-    const roomId = room.getId()
-    const msg = message.create({message: 'hola', roomId, user: 'lucas', date: Date.now()})
-    const feed1 = room.addUser('a')
-    const feed2 = room.addUser('b')
-    chat.sendMessage(msg)
-    feed1.on('message', () => {})
-    feed1.emit('message')
-    feed2.on('message', () => {})
+    chat.createRoom(timeout, (err, room) => {
+      const roomId = room.getId()
+      const msg = message.create({message: 'hola', roomId, user: 'lucas', date: Date.now()})
+      const feed1 = room.addUser('a')
+      const feed2 = room.addUser('b')
+      chat.sendMessage(msg)
+      feed1.on('message', () => {})
+      feed1.emit('message')
+      feed2.on('message', () => {})
+    })
   })
 })
--- test/lib/chat/chat.js
+++ test/lib/chat/chat.js
@@ -1,83 +1,117 @@
 const Chat = require('../../../lib/routes/chat')()
 const Room = require('../../../lib/routes/chat/room')
+const timeout = {timeoutInMillis: 30000}
 
 describe('lib/routes/chat.js', () => {
   it('should create a room', () => {
     const chat = new Chat()
-    const room = chat.createRoom({name: 'lucas'})
-    expect(room).to.be.instanceof(Room)
+    chat.createRoom(timeout, (err, room) => {
+      expect(err).to.be.null
+      expect(room).to.be.instanceof(Room)
+    })
   })
 
   it('should return an existing room', () => {
     const chat = new Chat()
-    const room = chat.createRoom({name: 'lucas'})
-    const result = chat.getRoom(room.getId())
-    expect(result)
-      .to.be.equal(room)
+    chat.createRoom(timeout, (err, room) => {
+      const result = chat.getRoom(room.getId())
+      expect(err).to.be.null
+      expect(result).to.be.equal(room)
+    })
   })
   it('should return a non existing room', () => {
     const chat = new Chat()
     const room = chat.getRoom(7)
-    expect(room)
-      .to.be.null
+    expect(room).to.be.null
   })
 
   it('should return created rooms', () => {
     const chat = new Chat()
-    const r1 = chat.createRoom({name: 'lucas'})
-    const r2 = chat.createRoom({name: 'lucas'})
-    const r3 = chat.createRoom({name: 'lucas'})
-    const rooms = chat.getRooms()
-    expect(rooms)
-      .to.be.an('array')
-      .to.have.length(3)
-      .to.have.members([r1, r2, r3])
+    let r1
+    let r2
+    let r3
+    chat.createRoom(timeout, (err1, room) => {
+      r1 = room
+      chat.createRoom(timeout, (err2, room) => {
+        r2 = room
+        chat.createRoom(timeout, (err3, room) => {
+          r3 = room
+          const rooms = chat.getRooms()
+          expect(err1).to.be.null
+          expect(err2).to.be.null
+          expect(err3).to.be.null
+          expect(rooms)
+            .to.be.an('array')
+            .to.have.length(3)
+            .to.have.members([r1, r2, r3])
+        })
+      })
+    })
   })
 
   it('should delete a room, existing', () => {
     const chat = new Chat()
-    const r1 = chat.createRoom({name: 'lucas'})
-    const r2 = chat.createRoom({name: 'lucas'})
-    const r3 = chat.createRoom({name: 'lucas'})
-    const removed = chat.removeRoom(r2)
-    expect(removed)
-      .to.be.equal(true)
-    const rooms = chat.getRooms()
-    expect(rooms)
-      .to.have.length(2)
-      .to.have.members([r1, r3])
+    let r1
+    let r2
+    let r3
+    chat.createRoom(timeout, (err1, room) => {
+      r1 = room
+      chat.createRoom(timeout, (err2, room) => {
+        r2 = room
+        chat.createRoom(timeout, (err3, room) => {
+          r3 = room
+          const removed = chat.removeRoom(r2)
+          expect(removed)
+            .to.be.equal(true)
+          const rooms = chat.getRooms()
+          expect(err1).to.be.null
+          expect(err2).to.be.null
+          expect(err3).to.be.null
+          expect(rooms)
+            .to.have.length(2)
+            .to.have.members([r1, r3])
+        })
+      })
+    })
   })
   it('should delete a room, non existing', () => {
     const chat1 = new Chat()
     const chat2 = new Chat()
-    const room = chat1.createRoom({name: 'lucas'})
-    const removed = chat2.removeRoom(room)
-    expect(removed)
-      .to.be.false
+    chat1.createRoom(timeout, (err, room) => {
+      const removed = chat2.removeRoom(room)
+      expect(err).to.be.null
+      expect(removed).to.be.false
+    })
   })
 
-  it('should send a msg to the specified room', () => {
+  it('should send a msg to the specified room', (done) => {
     const chat = new Chat()
-    const room = chat.createRoom({name: 'lucas'})
-    const feed = room.addUser({name:'lucas'})
-    const roomId = room.getId()
     let msgRecieved
-    feed.on('message', (message) => {
-      msgRecieved = message
+    chat.createRoom(timeout, (err, newRoom) => {
+      const room = newRoom
+      const feed = room.addUser({name:'lucas'})
+      const roomId = room.getId()
+      const message = { message: 'hola', roomId}
+      feed.on('message', (message) => {
+        msgRecieved = message
+      })
+      feed.start()
+      chat.sendMessage(message, roomId)
+      expect(err).to.be.null
+      expect(message)
+        .to.be.equal(msgRecieved)
+      return done()
     })
-    feed.start()
-    const message = { message: 'hola', roomId}
-    chat.sendMessage(message, roomId)
-    expect(message)
-      .to.be.equal(msgRecieved)
   })
   it('should throw on invalid msg', () => {
     const chat = new Chat()
-    const room = chat.createRoom('')
-    const roomId = room.getId()
-    const result = () => chat.sendMessage({ message: null, roomId })
-    expect(result)
-      .to.throw(/Invalid message, expected string but got undefined/)
+    chat.createRoom(timeout, (err, room) => {
+      const roomId = room.getId()
+      const result = () => chat.sendMessage({ message: null, roomId })
+      expect(err).to.be.null
+      expect(result)
+        .to.throw(/Invalid message, expected string but got undefined/)
+    })
   })
   it('should throw on invalid Id', () => {
     const chat = new Chat()
@@ -87,10 +121,12 @@ describe('lib/routes/chat.js', () => {
   })
   it('should throw on invalid Room Id', () => {
     const chat = new Chat()
-    const room = chat.createRoom('')
-    const roomId = room.getId()
-    const result = () => chat.sendMessage({message: 'hola', roomdId: roomId + 1})
-    expect(result)
-      .to.throw(/Invalid room ID/)
+    chat.createRoom(timeout, (err, room) => {
+      const roomId = room.getId()
+      const result = () => chat.sendMessage({message: 'hola', roomdId: roomId + 1})
+      expect(err).to.be.null
+      expect(result)
+        .to.throw(/Invalid room ID/)
+    })
   })
 })
